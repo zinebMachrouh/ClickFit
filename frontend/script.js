c2432956bee1d3be4a29cf86237ba502cc91cfe1
zinebMachrouh <zinebmac49@gmail.com>
2025-01-29 16:29:28 +0100

    Add Dockerfile for MySQL setup and add user page

--- frontend/script.js
+++ frontend/script.js
@@ -31,4 +31,46 @@ $(document).ready(function () {
         },
       });
     });
+
+    // User registration
+    $('#userForm').on('submit', function (e) {
+        e.preventDefault();
+        const email = $('#email').val().trim();
+        const password = $('#password').val().trim();
+        const type = $('#type').val();
+        const active = $('#status').val();
+
+        let isValid = true;
+
+        if (!email) {
+          $('#email-feedback').text('Email is required');
+          isValid = false;
+        } else {
+          $('#email-feedback').text('');
+        }
+
+        if (!password) {
+          $('#password-feedback').text('Password is required');
+          isValid = false;
+        } else {
+          $('#password-feedback').text('');
+        }
+
+
+        if (!isValid) return;
+
+        $.ajax({
+          url: 'http://localhost:3000/add-user',
+          type: 'POST',
+          contentType: 'application/json',
+          data: JSON.stringify({ email, password, type, active }),
+          success: function (response) {
+            $('#responseMessage').html('<div class="alert alert-success">' + response.message + '</div>');
+            $('#userForm')[0].reset();
+          },
+          error: function () {
+            $('#responseMessage').html('<div class="alert alert-danger">Failed to add user. Please try again.</div>');
+          }
+        });
+      });
   });
